c8fad64bc722b034d1346d7ecd6fc254c51be6a7
Ali <alirezaghorbani.bmedu@yahoo.com>
2026-05-13 10:31:21 +0200

    make a new table

---
+++ begin_mysql.sql
@@ -0,0 +1,29 @@
+## Agenda
+/*
+1.1. Introduction to Databases
+Definition: A database is an organized collection of data, typically stored 
+and accessed electronically from a computer system.
+Purpose: Databases store information in a structured way, 
+allowing efficient data retrieval, management, and manipulation.
+*/
+/*
+1. MySQL Create Databases
+2. MySQL Create Table without constraints
+3. MySQL Insert Data Into Table
+4. MySQL Drop Table
+5. MySQL DROP Databases
+6. Hands On excercises- Creating and Dropping Databases and Tables
+*/
+
+## create the database
+create database LibraryDb;
+
+create database ecommercedb;
+
+/*Best Practices:
+1. Naming Conventions: Use meaningful and consistent names (e.g., LibraryDB, ECommerceDB).
+2. Avoid Reserved Keywords: Ensure database names do not clash with MySQL reserved keywords.
+3. Use Lowercase Letters: While MySQL is case-insensitive on Windows, 
+it is case-sensitive on Unix-based systems. Consistency avoids confusion.*/
+
+
